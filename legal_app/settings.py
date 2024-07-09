e82fb3c665a9c06b5c12426e312516d8180ab509
Javier <javierpascalf@gmail.com>
2024-07-09 17:42:59 -0600

    add pending files

--- legal_app/settings.py
+++ legal_app/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'legal_app',
         'USER': 'postgres',
-        'PASSWORD': 'admin',
+        'PASSWORD': 'OmegaSpeedmaster',
         'HOST': '',
         'PORT': '5432'
 
